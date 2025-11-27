fbfcff87cca53ce095cc81253ad765d64bda897b
MateusPereiradev <dev.mateuspereira@gmail.com>
2025-11-27 20:36:54 -0300

    manipulando nome

--- nome.py
+++ nome.py
@@ -1,12 +1,10 @@
 nome= str(input('Digite aqui seu nome completo:')).strip()
-nome_maiusculo= nome.upper()
-print(f'O nome em letras maiúsculas é {nome_maiusculo}')
-nome_minusculo= nome.lower()
-print(f'O nome em letras minúsculas é {nome_minusculo}')
-letras= len(nome.replace(' ', ''))
-print(f'A contagem do nome completo, retirando os espaços é de {letras}')
-nome_lista= nome.split()
-print(f'O primeiro nome contém {len(nome_lista[0])} letras')
+print(f'O nome com todas as letras maiúsculas é {nome.upper()}')
+print(f'O nome com todas as letras minúsculas é {nome.lower()}')
+print(f'O total de letras sem considerar espações é {len(nome.replace(' ', ''))}')
+lista_nome= nome.split()
+print(f'O primeiro nome tem {len(lista_nome[0])} letras!')
+
 '''
 Crie um programa que leia o nome completo de uma pessoa e mostre:
 O nome com todas as letras maiúsculas;
