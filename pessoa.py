93f324772feff8f2f9a192022444395523430562
mateuspereiradev <dev.mateuspereira@gmail.com>
2025-12-04 20:35:29 -0300

    Tem SILVA em pesso?

--- pessoa.py
+++ pessoa.py
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
-nome= str(input("Digite aqui seu nome completo:")).strip().upper()
-print("SILVA" in nome)
-
-=======
 nome= str(input('Digite aqui seu nome completo:')).strip().upper()
-print(f'O nome tem em algum lugar SILVA? {'SILVA' in nome}')
->>>>>>> 1100eb16bc928c721ad2aff157d52e53e7b33ee8
+print(f'Existe SILVA no nome? {'SILVA' in nome}')
 
 '''
 Crie um programa que leia o nome de uma pessoa e diga se ela tem 'SILVA' no nome.
