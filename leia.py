37143e128ea4038d32d760c8e9883b6405543024
mateuspereiradev <dev.mateuspereira@gmail.com>
2025-12-04 20:28:18 -0300

    Primeiro e último nome do nome informado

--- leia.py
+++ leia.py
@@ -1,14 +1,9 @@
-<<<<<<< HEAD
-nome= str(input("Informe aqui seu nome completo:")).strip()
-nome_lista= nome.split()
-print(f"O primeiro nome é? {nome_lista[0]}")
-print(f"O último nome é? {nome_lista[-1]}")
-=======
 nome= str(input('Digite aqui seu nome completo:')).strip()
-lista_nome= nome.split()
-print(f'Referente ao nome informado, o primeiro nome é {lista_nome[0]}')
-print(f'Referente ao nome informado, o último nome é {lista_nome[-1]}')
->>>>>>> 1100eb16bc928c721ad2aff157d52e53e7b33ee8
+nome_lista= nome.split()
+print(f'O primeiro nome é {nome_lista[0]}')
+print(f'O último nome é {nome_lista[-1]}')
+
+
 '''
 Faça um programa que leia o nome completo de uma pessoa, mostrando
 em seguida o primeiro e o último nome separadamente.
