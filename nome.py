a4effe8c9304ef73a9201d1cca67ffd345c6058e
mateuspereiradev <dev.mateuspereira@gmail.com>
2025-12-08 19:47:41 -0300

    detalhes de um nome

--- nome.py
+++ nome.py
@@ -1,9 +1,10 @@
-nome= str(input('Digite aqui seu nome completo:')).strip()
-print(f'O nome com todas as letras maiúsculas é {nome.upper()}')
-print(f'O nome com todas as letras minúsculas é {nome.lower()}')
-print(f'Quanta letras ao todo o nome tem? {len(nome.replace(' ', ''))}')
+nome= str(input('Digite aqui seu nome completo:')).strip().upper()
+print(f'O nome com todas as letras maiúsculas:{nome}')
+print(f'O nome com todas as letras minúsculas:{nome.lower()}')
+print(f'Quantas letras tem o nome sem considerar os espaços? {len(nome.replace(' ', ''))}')
 nome_lista= nome.split()
 print(f'Quantas letras tem o primeiro nome? {len(nome_lista[0])}')
+
 
 '''
 Crie um programa que leia o nome completo de uma pessoa e mostre:
