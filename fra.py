329676c84b4fa78707205f1da02ad870951df1dd
mateuspereiradev <dev.mateuspereira@gmail.com>
2025-12-08 19:37:34 -0300

    Quantas letras A, quando ela aparece primeiro e por último

--- fra.py
+++ fra.py
@@ -1,8 +1,7 @@
 frase= str(input('Digite aqui uma frase:')).strip().upper()
 print(f'Quantas vezes aparece a letra A? {frase.count('A')}')
-print(f'Em que posição ela aparece pela primeira vez? {frase.find('A')+1}')
-print(f'Em que posição ela aparece pela última vez? {frase.rfind('A')+1}')
-
+print(f'Em que posição a letra A aparece primeiro? {frase.find('A')+1}')
+print(f'Em que posição a letra A aparece por último? {frase.rfind('A')+1}')
 '''
 Faça um programa que leia uma frase pelo teclado e mostre:
 >Quantas vez aparece a letra 'A'
