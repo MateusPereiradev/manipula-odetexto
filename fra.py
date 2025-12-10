6bf76dd950e39be969d957b709ef858ce55ab5bd
mateuspereiradev <dev.mateuspereira@gmail.com>
2025-12-10 20:06:41 -0300

    manipulando frase

--- fra.py
+++ fra.py
@@ -1,7 +1,9 @@
 frase= str(input('Digite aqui uma frase:')).strip().upper()
 print(f'Quantas vezes aparece a letra A? {frase.count('A')}')
-print(f'Em que posição a letra A aparece primeiro? {frase.find('A')+1}')
-print(f'Em que posição a letra A aparece por último? {frase.rfind('A')+1}')
+print(f'Em que posição ela aparece a primeira vez? {frase.find('A')+1}')
+print(f'Em que posição ela aparece a última vez? {frase.rfind('A')+1}')
+
+
 '''
 Faça um programa que leia uma frase pelo teclado e mostre:
 >Quantas vez aparece a letra 'A'
