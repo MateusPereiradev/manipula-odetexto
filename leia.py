22b3af18a653828c13b4a9b38f6527868b0cd4d6
mateuspereiradev <dev.mateuspereira@gmail.com>
2025-12-10 20:13:45 -0300

    Manipulando nome

--- leia.py
+++ leia.py
@@ -1,8 +1,7 @@
 nome= str(input('Digite aqui seu nome completo:')).strip().upper().split()
-print(f'Qual é o primeiro nome? {nome[0]}')
-print(f'Qual é o último nome? {nome[-1]}')
-
-
+print(f'Qual o primeiro nome? {nome[0]}')
+print(f'Qual o último nome? {nome[-1]}')
+print('Fim do programa!')
 '''
 Faça um programa que leia o nome completo de uma pessoa, mostrando
 em seguida o primeiro e o último nome separadamente.
